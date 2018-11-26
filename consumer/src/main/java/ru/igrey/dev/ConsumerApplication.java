634ce47d80a3d23e3d4b39acbd50d784db86b58b
sanasov <anasovss@msk.bcs.ru>
2018-11-26 12:37:44 +0300

    xml converter

--- consumer/src/main/java/ru/igrey/dev/ConsumerApplication.java
+++ consumer/src/main/java/ru/igrey/dev/ConsumerApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.annotation.StreamMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.messaging.converter.MarshallingMessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import ru.igrey.dev.model.LogMessage;
 import ru.igrey.dev.processor.MyProcessor;
 
@@ -30,5 +37,19 @@ public class ConsumerApplication {
             throw new RuntimeException("Error message:" + msg);
         }
         System.out.println(msg);
+    }
+
+    @Bean
+    @StreamMessageConverter
+    public MessageConverter xmlConverter() {
+        return new MarshallingMessageConverter(marshaller());
+    }
+
+    @Bean
+    public Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setClassesToBeBound(LogMessage.class);
+        marshaller.setSchemas(new ClassPathResource("logMessage.xsd"));
+        return marshaller;
     }
 }
--- consumer/src/main/java/ru/igrey/dev/model/LogMessage.java
+++ consumer/src/main/java/ru/igrey/dev/model/LogMessage.java
@@ -1,19 +1,32 @@
 package ru.igrey.dev.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class LogMessage implements Serializable {
 
     private static final long serialVersionUID = -5857383701708275796L;
 
     @XmlElement
     private String message;
+    @XmlElement
+    private Integer age;
 
     public LogMessage() {
 
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
     public LogMessage(String message) {
